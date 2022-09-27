0b6f9f5ca279ab260e63d506c3c1dafe0753cf47
Jim Etevenard <jetevenard@oxiane.com>
2022-09-27 15:55:54 +0200

    Gestion des promise et cas d'erreur

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 const pickerOpts = {
     types: [
-      {
-        description: 'Images',
-        accept: {
-          'text/*': ['.txt', '.md', '.html', '.htm', '.xml', '.json']
-        }
-      },
+        {
+            description: 'Images',
+            accept: {
+                'text/*': ['.txt', '.md', '.html', '.htm', '.xml', '.json']
+            }
+        },
     ],
     excludeAcceptAllOption: true,
     multiple: false
-  };
-  
-  async function getTheFile() {
-    // open file picker
-    // NB : entre crochet car showOpenFilePicker retourne un array : on pourrait avoir plusieurs fichiers
-    // Ex. [foo] = ["foo", "bar", "baz"];
-    [fileHandle] = await window.showOpenFilePicker(pickerOpts);
-  
-    // get file contents
-    const fileData = await fileHandle.getFile();
-    console.log(fileData);
+};
 
+function getTheFile() {
+    // open file picker
+    window.showOpenFilePicker(pickerOpts).then(function ([fileHandle]) {
+
+        // get file contents
+        fileHandle.getFile().then(handleFileData, handleError);
+
+    }, handleError);
+}
+
+function handleFileData(fileData) {
     const metadata = `${fileData.name} - modifié le ${fileData.lastModifiedDate}`;
     document.getElementById('metadata').innerText = metadata;
 
-    const contents = await fileData.text()
+    fileData.text().then(displayFileContents, handleError);
+}
+
+function displayFileContents(contents){
     document.getElementById('editor').value = contents;
-  }
-  
-  document.getElementById('btn-open')
-     .addEventListener('click', getTheFile);
+}
+
+function handleError(error) {
+    alert(`ERREUR : ${error}`);
+    console.error(error);
+}
+
+document.getElementById('btn-open').addEventListener('click', getTheFile);
